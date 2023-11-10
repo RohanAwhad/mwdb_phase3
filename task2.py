ec09eca5fe0fa8e1911e8a3ffbac8bdabcdada3a
Rohan Awhad <rohanavad007@gmail.com>
2023-11-10 11:02:13 -0700

    added task as docstring

---
+++ task2.py
@@ -0,0 +1,11 @@
+'''
+Task 2: Implement a program which,
+
+  - for each unique label l, computes the correspending c most significant clusters associated with the even numbered Caltec101 images (using DBScan algorithm); the resulting clusters should be visualized both
+    * as differently colored point clouds in a 2-dimensional MDS space, and
+    * as groups of image thumbnails. and 
+  - for the odd numbered images, predicts the most likely labels using the c label-specific clusters.
+
+The system should also output per-label precision, recall, and F1-score values as well as output an overall accuracy value.
+
+'''
