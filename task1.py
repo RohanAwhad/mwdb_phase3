d25c4eb045beb9435d3ad52fc3db7c686ccc7f61
Rohan Awhad <rohanavad007@gmail.com>
2023-11-27 15:34:04 -0700

    Update latent feature computation and prediction

--- task1.py
+++ task1.py
@@ -27,6 +27,7 @@ Questions:
 
 1. Do you want the output to be csv or print it on terminal?
 """
+from collections import Counter
 import numpy as np
 import os
 import torch
@@ -107,9 +108,8 @@ class SVD:
 
 
 extractor = FeatureExtractor()
-svd = SVD()
-K = input("Enter the number of latent dimensions (K. Default is 512): ")
-K = 512 if K == "" else int(K)
+K = input("Enter the number of latent dimensions (K. Default is 5): ")
+K = 5 if K == "" else int(K)
 
 if os.path.exists("artifacts/resnet50_avgpool_features.pkl"):
     features = torch.load("artifacts/resnet50_avgpool_features.pkl")
@@ -138,39 +138,71 @@ else:
     torch.save(labels, "artifacts/labels.pkl")
     torch.save(image_ids, "artifacts/image_ids.pkl")
 
-IMG_LATENT_FEAT_MAP_PATH = f"artifacts/img_latent_feat_map_K_{K}.pkl"
-IMG_LATENT_FEAT_COMPONENTS_PATH = f"artifacts/img_latent_feat_components_K_{K}.pkl"
+IMG_LATENT_FEAT_MAP_PATH = f"artifacts/task_1_new_img_latent_feat_map_K_{K}.pkl"
+IMG_LATENT_FEAT_COMPONENTS_PATH = (
+    f"artifacts/task_1_new_img_latent_feat_components_K_{K}.pkl"
+)
 if os.path.exists(IMG_LATENT_FEAT_MAP_PATH):
-    latent_features = torch.load(IMG_LATENT_FEAT_MAP_PATH)
-    svd.components_ = torch.load(IMG_LATENT_FEAT_COMPONENTS_PATH)
+    label_to_latent_features = torch.load(IMG_LATENT_FEAT_MAP_PATH)
+    label_to_svd = torch.load(IMG_LATENT_FEAT_COMPONENTS_PATH)
 else:
-    latent_features = svd.fit_transform(features, K)
-    torch.save(latent_features, IMG_LATENT_FEAT_MAP_PATH)
-    torch.save(svd.components_, IMG_LATENT_FEAT_COMPONENTS_PATH)
+    # get indices for each label
+    label_indices = {}
+    for label in tqdm(set(labels), desc="Getting Label Indices", leave=False):
+        label_indices[label] = [i for i, y in enumerate(labels) if y == label]
+
+    # get latent features for each label
+    label_to_latent_features = {}
+    label_to_svd = {}
+    for label, indices in tqdm(
+        label_indices.items(), desc="Computing Latent Features", leave=False
+    ):
+        _tmp = features[indices]
+        svd = SVD()
+        label_to_latent_features[label] = svd.fit_transform(_tmp, K)
+        label_to_svd[label] = svd
+
+    torch.save(label_to_latent_features, IMG_LATENT_FEAT_MAP_PATH)
+    torch.save(label_to_svd, IMG_LATENT_FEAT_COMPONENTS_PATH)
 
 
-LABEL_LATENT_FEAT_MAP_PATH = f"artifacts/label_latent_feat_map_K_{K}.pkl"
-if os.path.exists(LABEL_LATENT_FEAT_MAP_PATH):
-    label_latent_feat_map = torch.load(LABEL_LATENT_FEAT_MAP_PATH)
+def cosine_similarity(x, y):
+    if x.ndim == 1:
+        x = x.unsqueeze(0)
+    if y.ndim == 1:
+        y = y.unsqueeze(0)
+    return (x @ y.T) / (torch.norm(x) * torch.norm(y))
+
+
+def predict_label(img_feat, label_to_svd, label_to_latent_features):
+    similarities = []
+    all_labels = []
+    for label, svd in label_to_svd.items():
+        latent_features = label_to_latent_features[label]
+        simi = cosine_similarity(svd.transform(img_feat), latent_features).squeeze()
+        similarities.append(simi)
+        all_labels.extend([label] * len(simi))
+
+    similarities = torch.cat(similarities)
+    all_labels = torch.tensor(all_labels)
+
+    # argsort and select top 10 labels
+    top_10_indices = torch.argsort(similarities)[-10:]
+    top_10_labels = all_labels[top_10_indices]
+
+    # find the most common label
+    label_counts = Counter(top_10_labels)
+    predicted_label = label_counts.most_common(1)[0][0]
+    return predicted_label
+
+
+PRED_FN = f"artifacts/task_1_new_preds_K_{K}.pkl"
+TRUES_FN = f"artifacts/task_1_new_trues_K_{K}.pkl"
+if os.path.exists(PRED_FN) and os.path.exists(TRUES_FN):
+    preds = torch.load(PRED_FN)
+    trues = torch.load(TRUES_FN)
 else:
-    label_latent_feat_map = torch.empty((len(set(DATASET.y)), K))
-
-    for label in tqdm(set(labels), desc="Computing Label Latent Features", leave=False):
-        _tmp = [latent_features[i] for i, y in enumerate(labels) if y == label]
-        label_latent_feat_map[label] = torch.stack(_tmp).mean(0).flatten()
-
-    torch.save(label_latent_feat_map, LABEL_LATENT_FEAT_MAP_PATH)
-
-
-preds = []
-
-ODD_IMG_LATENT_FEAT_PATH = f"artifacts/odd_img_latent_feat_K_{K}.pkl"
-ODD_IMG_LABELS_PATH = f"artifacts/odd_image_labels_K_{K}.pkl"
-if os.path.exists(ODD_IMG_LATENT_FEAT_PATH):
-    odd_image_latent_features = torch.load(ODD_IMG_LATENT_FEAT_PATH)
-    trues = torch.load(ODD_IMG_LABELS_PATH)
-else:
-    odd_image_latent_features = []
+    preds = []
     trues = []
     # calculate and save odd image latent features
     for idx, (image, label) in tqdm(
@@ -185,27 +217,15 @@ else:
         if image.mode != "RGB":
             image = image.convert("RGB")
         feature = extractor(image)
-        latent_feature = svd.transform(feature)
-        odd_image_latent_features.append(latent_feature.squeeze())
+        preds.append(predict_label(feature, label_to_svd, label_to_latent_features))
         trues.append(label)
 
-    odd_image_latent_features = torch.stack(odd_image_latent_features)
     trues = torch.tensor(trues)
-    torch.save(odd_image_latent_features, ODD_IMG_LATENT_FEAT_PATH)
-    torch.save(trues, ODD_IMG_LABELS_PATH)
+    preds = torch.tensor(preds)
 
+    torch.save(preds, PRED_FN)
+    torch.save(trues, TRUES_FN)
 
-for latent_feature in tqdm(
-    odd_image_latent_features, desc="Predicting Labels", leave=False
-):
-    # Calculating cosine similarity
-    cosine_sim = (latent_feature @ label_latent_feat_map.T) / (
-        torch.norm(latent_feature) * torch.norm(label_latent_feat_map, dim=1)
-    )
-    pred = torch.argmax(cosine_sim)
-    preds.append(pred)
-
-preds = torch.tensor(preds)
 
 # Calculating per-label metrics
 per_label_metrics = {}
@@ -230,7 +250,7 @@ precision = tp / (tp + fp)
 accuracy = tp / len(preds)
 
 
-with open(f"outputs/task1_K_{K}.csv", "w") as f:
+with open(f"outputs/task1_new_K_{K}.csv", "w") as f:
     f.write("label,precision,recall,f1_score\n")
     for label, metrics in per_label_metrics.items():
         f.write(
